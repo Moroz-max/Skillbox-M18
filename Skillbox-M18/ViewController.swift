cb5ffeaee323a6b2ff45fea3e4f0242268279eb8
Maxim <moroz@nonstudio.ru>
2023-12-22 15:47:25 +0300

    Initial Commit

---
+++ Skillbox-M18/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Skillbox-M18
+//
+//  Created by Максим Морозов on 22.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
